3e18df8f96577be71064cf2241f853cb74a27915
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2024-01-04 09:50:10 +0000

    first commit

---
+++ my-to-do/src/App.tsx
@@ -0,0 +1,13 @@
+import Nav from "./containers/Nav"
+
+const App= () => {
+  
+
+  return (
+    <Nav/>
+   
+ 
+  )
+}
+
+export default App
---
+++ my-to-do/src/containers/Nav.tsx
@@ -0,0 +1,11 @@
+
+
+const Nav = () =>{
+
+    return(
+        <p> Hello</p>
+    )
+        
+    
+}
+export default Nav ;
